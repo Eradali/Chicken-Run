ef94ee6ea23b4e5c61bed40b0ca31aeef4e618ac
Erad Ali <eradali34@yahoo.com>
2022-10-02 19:04:51 +0500

    Touch player controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    float input1;
+    //cached ref
+    [SerializeField] Animator chickenAnimator;
+
+    //config para
+    [SerializeField] float input1 = 0;
+    [SerializeField] float runningSpeed;
+    float turnValue = 0f;
+    [SerializeField] float turningSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        chickenAnimator = GetComponent<Animator>();
+        chickenAnimator.SetBool("Run", true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * 6f);
+        transform.Translate(Vector3.forward * Time.deltaTime * runningSpeed);       //moving forward
 
-        input1 = Input.GetAxis("Horizontal");
-        transform.Translate(new Vector3(input1, 0, 0) * Time.deltaTime * 5f);
+        transform.Rotate(0f, turnValue * turningSpeed, 0f);     //turning left & right
+
+        //input1 = Input.GetAxis("Horizontal");
+        //transform.Translate(new Vector3(input1, 0, 0) * Time.deltaTime * turningSpeed);       //turning left & right using keyboard
     }
+
+    public void SetTurnValue(float value)
+    {
+        turnValue = value;
+    }
+
 }
